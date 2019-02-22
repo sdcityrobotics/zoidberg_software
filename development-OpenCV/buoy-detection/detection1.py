6d4b2dbf6f112b6015b17f11c041258c8ef19ee5
smithjoana <38543886+smithjoana@users.noreply.github.com>
2019-02-21 20:23:28 -0800

    Was doing some of the math wrong - fixed it

--- development-OpenCV/buoy-detection/detection1.py
+++ development-OpenCV/buoy-detection/detection1.py
@@ -5,8 +5,10 @@ import cv2
 # images for testing:
 # from comp's video: '001.png', '002.png', '003.png', 004.png', '005.png'
 # external buoy's: '010.jpg', ...
-img = cv2.imread('002.png', 0)
-img_color = cv2.imread('002.png')
+img_text = '004.png'
+img = cv2.imread(img_text, 0)
+img_color = cv2.imread(img_text)
+img_allcontours = cv2.imread(img_text)
 
 # import sobel function to convert image (derivative WRT y)
 # ksize value influences noise levels
@@ -21,60 +23,55 @@ thresh_y = cv2.dilate(thresh_y, None, iterations = 1)
 contours = cv2.findContours(thresh_y.copy(), cv2.RETR_EXTERNAL, cv2. CHAIN_APPROX_SIMPLE)
 contours = contours[1];
 
-ratioMax = 2.0
-ratioMin = 0.5
+ratioMax = 0.5
+ratioMin = 0.2
 
 buoys = []
-count = 0 
+count = 0
+
 for contour in contours:
-   if (len(contour) > 20):
-      #find the highest point in y (will have the lowest valie at [0])
+   cv2.drawContours(img_allcontours, [contour], -1, (255, 255, 0), 2)
+   if (len(contour) > 45):
+      #find the highest point in y 
       highestPt = [9999999, 9999999] #set to be just really high
-      endPt1 = [0, 9999999999] #will be pt with lowest [1]
-      endPt2 = [0,-1] #will be pt with highest [1]
-    
+      endPt1 = [9999999999, 0] #will be pt with lowest x 
+      endPt2 = [-1, 0] #will be pt with highest x
+
       for Point in contour:
-         tempy = Point[0][0]
-         tempx = Point[0][1]
-         if (tempy < highestPt[0]):
+         tempy = Point[0][1]
+         tempx = Point[0][0]
+         if (tempy < highestPt[1]):
             highestPt = Point[0]
-         if (tempx < endPt1[1]):
+         if (tempx < endPt1[0]):
             endPt1 = Point[0]
-         if (tempx > endPt2[1]):
+         if (tempx > endPt2[0]):
             endPt2 = Point[0]
 
-      print('endPoint1', endPt1)
-      print('endPoint2', endPt2)
-      print('highestPt', highestPt)
-
-      #pick the endpoint highest in y (so lowest [0]
+      #pick the endpoint highest in y
       finalEndPt = endPt1;
-      if (endPt1[0] > endPt2[0]):
+      if (endPt1[1] > endPt2[1]):
          finalEndPt = endPt2;
 
-      w = abs(highestPt[1] - finalEndPt[1])
-      h = abs(highestPt[0] - finalEndPt[0])
+      w = abs(highestPt[0] - finalEndPt[0])
+      h = abs(highestPt[1] - finalEndPt[1])
       if (w != 0):
          ratio = (h/(2*w))
       else: #safeguard against dividing by 0
          ratio = 0
-      if (endPt1[0]==endPt2[0] and endPt1[1]==endPt2[1]):
-         ratio = 0
 
-      print('ratio', ratio)
-      print()
-      if ((ratio > ratioMin) and (ratio < ratioMax)):
+      if ((ratio >= ratioMin) and (ratio <= ratioMax)):
+         print('finalEndPt', finalEndPt)
+         print('highestPt', highestPt)
          print('valid ratio: ', ratio)
+         print()
          buoys.append(contour)
          count = count+1
 
 print('total valid ratios: ', count)
-#print('valid contours: ', buoys)
 
 # display
-for buoy in buoys:
-   cv2.drawContours(img_color, [buoy], -1, (255, 255, 0), 2)
 cv2.imshow('Contours', img_color)
+#cv2.imshow('all contours', img_allcontours)
 
 cv2.waitKey(0)
-cv2.destroyAllWindows()                                                                                                                                                           1,1           Top
+cv2.destroyAllWindows()
